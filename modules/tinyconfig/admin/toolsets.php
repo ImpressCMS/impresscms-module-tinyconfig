af4f1bfa07defec33fb2048b76924b71739a2cc2
Ronald <pietjebell31@hotmail.com>
2012-03-23 22:09:50 +0000

    Importing TinyConfig
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/tinyconfig/trunk@23939 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ modules/tinyconfig/admin/toolsets.php
@@ -0,0 +1,142 @@
+<?php
+/**
+* TinyConfig
+*
+* File: /admin/index.php
+*
+* @copyright	http://www.xoops.org/ The XOOPS Project
+* @copyright	XOOPS_copyrights.txt
+* @copyright	http://www.impresscms.org/ The ImpressCMS Project
+* @license		GNU General Public License (GPL)
+*				a copy of the GNU license is enclosed.
+* ----------------------------------------------------------------------------------------------------------
+* 				TinyConfig
+* @since		1.00
+* @author		McDonald
+* @version		$Id$
+*/
+
+include 'admin_header.php';
+
+$op = '';
+
+if ( isset( $_GET['op'] ) ) $op = $_GET['op'];
+if ( isset( $_POST['op'] ) ) $op = $_POST['op'];
+
+if ( isset( $_GET['tinycfg_gid'] ) ) { $tinycfg_gid = $_GET['tinycfg_gid']; } else { $tinycfg_gid = 0; }
+if ( isset( $_POST['tinycfg_gid'] ) ) { $tinycfg_gid = $_POST['tinycfg_gid']; } else { $tinycfg_gid = 0; }
+
+function toolsets_edit( $tinycfg_gid = 0 ) {
+	global $icmsConfig, $icmsAdminTpl;
+
+	if (!empty($_POST['grouplist'])) {
+
+	echo '<br />';
+
+	$sql = 'SELECT * FROM ' . icms::$xoopsDB -> prefix( 'tinycfg_toolsets' )." WHERE tinycfg_gid = ".intval($_POST['grouplist']);
+	$tinycfg_array = icms::$xoopsDB -> fetchArray( icms::$xoopsDB -> query( $sql ) );
+
+	if ( !$tinycfg_array['tinycfg_gid'] ) {
+		$tinycfg_gid = '';
+	} 
+
+	$tinycfg_id		= $tinycfg_array['tinycfg_id'] ? $tinycfg_array['tinycfg_id'] : 0;
+	$plugins		= $tinycfg_array['plugins'] ? htmlspecialchars( $tinycfg_array['plugins'] ) : 'icmsmlcontent,xoopsimagemanager,xoopsquotecode,xoopsemotions,table,advimage,advlink,emotions,insertdatetime,preview,media,contextmenu,paste,fullscreen,visualchars,nonbreaking';
+	$buttons1		= $tinycfg_array['buttons1'] ? htmlspecialchars( $tinycfg_array['buttons1'] ) : 'bold,italic,underline,strikethrough,sub,sup,separator,justifyleft,justifycenter,justifyright,justifyfull,formatselect,fontselect,fontsizeselect';
+	$buttons2		= $tinycfg_array['buttons2'] ? htmlspecialchars( $tinycfg_array['buttons2'] ) : 'xoopsquote,xoopscode,icmsmlcontent,separator,bullist,numlist,separator,outdent,indent,separator,undo,redo,removeformat,separator,link,unlink,anchor,xoopsimagemanager,media,separator,charmap,nonbreaking,hr,xoopsemotions,separator,pastetext,pasteword,separator,forecolor,backcolor';
+	$buttons3		= $tinycfg_array['buttons3'] ? htmlspecialchars( $tinycfg_array['buttons3'] ) : 'tablecontrols,separator,cleanup,visualaid,visualchars,separator,insertdate,inserttime,separator,preview,fullscreen,help,code';
+	$statusbar		= $tinycfg_array['statusbar'] ? htmlspecialchars( $tinycfg_array['statusbar'] ) : 'bottom';
+	$resize			= $tinycfg_array['resize'] ? htmlspecialchars( $tinycfg_array['resize'] ) : 'true';
+	
+	if ( !$tinycfg_array['tinycfg_gid'] ) {
+		echo '<div style="border: 1px solid #d9d9d9; margin-bottom: 10px; padding: 5px;">' . _AM_TINYCFG_TOOLSETS_NOTSET . '</div>';
+	}
+
+	$sform = new icms_form_Theme( _AM_TINYCFG_TOOLSETSMCE, 'storyform', 'toolsets.php' );
+	$sform -> setExtra( 'enctype="multipart/form-data"' );
+
+	$pluginz = new icms_form_elements_Textarea( _AM_TINYCFG_PLUGINS, 'plugins', $plugins, 7, 60);
+	$sform -> addElement( $pluginz, false );
+
+	$buttonsa = new icms_form_elements_Textarea( _AM_TINYCFG_BUTTONB1, 'buttons1', $buttons1, 7, 60);
+	$sform -> addElement( $buttonsa, false );
+
+	$buttonsb = new icms_form_elements_Textarea( _AM_TINYCFG_BUTTONB2, 'buttons2', $buttons2, 7, 60);
+	$sform -> addElement( $buttonsb, false );
+
+	$buttonsc = new icms_form_elements_Textarea( _AM_TINYCFG_BUTTONB3, 'buttons3', $buttons3, 7, 60);
+	$sform -> addElement( $buttonsc, false );
+
+	$status_array = array( 'none'=>_AM_TINYCFG_STATUSB_NONE, 'bottom'=>_AM_TINYCFG_STATUSB_BOTTOM, 'top'=>_AM_TINYCFG_STATUSB_TOP );
+	$status_select = new icms_form_elements_Select( _AM_TINYCFG_STATUSB, 'statusbar', $statusbar );
+	$status_select -> addOptionArray( $status_array );
+	$sform -> addElement( $status_select );
+
+	$resize_array = array( 'true'=>_AM_TINYCFG_TRUE, 'false'=>_AM_TINYCFG_FALSE );
+	$resize_select = new icms_form_elements_Select( _AM_TINYCFG_RESIZE, 'resize', $resize );
+	$resize_select -> addOptionArray( $resize_array );
+	$sform -> addElement( $resize_select );
+
+
+	$sform -> addElement( new icms_form_elements_Hidden( 'tinycfg_gid', intval($_POST['grouplist'] ) ) );
+	$sform -> addElement( new icms_form_elements_Hidden( 'tinycfg_id', $tinycfg_id ) );
+
+	$button_tray = new icms_form_elements_Tray( '', '' );
+	$hidden = new icms_form_elements_Hidden( 'op', 'save' );
+	$button_tray -> addElement( $hidden );
+
+	$butt_create = new icms_form_elements_Button( '', '', _SUBMIT, 'submit' );
+	$butt_create -> setExtra( 'onclick="this.form.elements.op.value=\'save_toolsets\'"' );
+	$button_tray -> addElement( $butt_create );
+	$sform -> addElement( $button_tray );
+	$sform -> display();
+	unset( $hidden ); 
+
+	}
+
+icms_cp_footer();
+}
+
+switch ( strtolower( $op ) ) {
+	case 'edit':
+	default:
+		icms_cp_header();
+		icms::$module -> displayAdminMenu( 3, _AM_TINYCFG_TOOLSETS . ' | TinyConfig' );
+
+		warnings();
+
+		$groupform = new icms_form_Theme( _AM_TINYCFG_GROUPS, 'grouplist', 'toolsets.php' );
+		$group_tray = new icms_form_elements_Tray( _AM_TINYCFG_GROUPSELECT, '' );
+		$group_tray -> addElement( new icms_form_elements_select_Group( '', 'grouplist', true, '', 1, false ) );
+		$group_tray -> addElement( new icms_form_elements_Button( '', 'grouplistsubmit', _GO, 'submit' ) );
+		$groupform -> addElement( $group_tray );
+		$groupform -> display();
+
+		toolsets_edit( $tinycfg_gid );
+		break;
+
+	case 'save_toolsets':
+		$tinycfg_id = ( !empty( $_POST['tinycfg_id'] ) ) ? $_POST['tinycfg_id'] : 0;
+		$plugins	= icms_core_DataFilter::addSlashes( trim( $_POST['plugins'] ) );
+		$buttons1	= icms_core_DataFilter::addSlashes( trim( $_POST['buttons1'] ) );
+		$buttons2	= icms_core_DataFilter::addSlashes( trim( $_POST['buttons2'] ) );
+		$buttons3	= icms_core_DataFilter::addSlashes( trim( $_POST['buttons3'] ) );
+		$statusbar	= addslashes( trim( $_POST['statusbar'] ) );
+		$resize		= addslashes( trim( $_POST['resize'] ) );
+		$tinycfg_gid	= $_POST['tinycfg_gid'];
+	
+		if ( !$tinycfg_id ) {
+		
+			$sql = "INSERT INTO " . icms::$xoopsDB -> prefix( 'tinycfg_toolsets' ) . "(tinycfg_id, tinycfg_gid, plugins, buttons1, buttons2, buttons3, statusbar, resize)";
+			$sql .= " VALUES ('', '$tinycfg_gid', '$plugins', '$buttons1', '$buttons2', '$buttons3', '$statusbar', '$resize')";
+		} else {
+			$sql = "UPDATE " . icms::$xoopsDB -> prefix( 'tinycfg_toolsets' ) . " SET plugins='$plugins', buttons1='$buttons1', buttons2='$buttons2', buttons3='$buttons3', statusbar='$statusbar', resize='$resize' WHERE tinycfg_gid = " . $tinycfg_gid;
+		}
+		if ( !$result = icms::$xoopsDB -> queryF( $sql ) ) {
+			icms::$logger -> handleError( E_USER_WARNING, $sql, __FILE__, __LINE__ );
+			return false;
+		}
+		redirect_header( 'toolsets.php', 2, _AM_TINYCFG_CONFIGSAVE );
+		break;
+}
+?>
---
+++ modules/tinyconfig/language/english/admin.php
@@ -0,0 +1,76 @@
+<?php
+/**
+* TinyConfig
+*
+* File: /language/english/admin.php
+*
+* @copyright	http://www.xoops.org/ The XOOPS Project
+* @copyright	XOOPS_copyrights.txt
+* @copyright	http://www.impresscms.org/ The ImpressCMS Project
+* @license		GNU General Public License (GPL)
+*				a copy of the GNU license is enclosed.
+* ----------------------------------------------------------------------------------------------------------
+* 				TinyConfig
+* @since		1.00
+* @author		McDonald
+* @version		$Id$
+*/
+
+define( '_AM_TINYCFG_WARNINSTALL', 'WARNING: Folder %s does not exists on your server. <br />This folder is required by TinyConfig.' );
+
+
+define( '_AM_TINYCFG_TOOLSETS', 'Toolsets' );
+define( '_AM_TINYCFG_TOOLSETSMCE', 'TinyMCE Toolset' );
+define( '_AM_TINYCFG_TOOLSETS_NOTSET', '<b>This group has no toolset yet!</b><br />The values you see are default values. Please edit and submit the form to create a toolset for this group.' );
+define( '_AM_TINYCFG_TOOLSAVE', 'Toolset for TinyMCE editor have been saved.' );
+
+define( '_AM_TINYCFG_GROUPS', 'Groups' );
+define( '_AM_TINYCFG_GROUPSELECT', 'Select a group' );
+
+define( '_AM_TINYCFG_PLUGINS', 'Plugins' );
+define( '_AM_TINYCFG_BUTTONB1', 'Button bar 1' );
+define( '_AM_TINYCFG_BUTTONB2', 'Button bar 2' );
+define( '_AM_TINYCFG_BUTTONB3', 'Button bar 3' );
+define( '_AM_TINYCFG_BUTTONB4', 'Button bar 4' );
+define( '_AM_TINYCFG_STATUSB', 'Elements path location' );
+define( '_AM_TINYCFG_STATUSB_BOTTOM', 'bottom' );
+define( '_AM_TINYCFG_STATUSB_TOP', 'top' );
+define( '_AM_TINYCFG_STATUSB_NONE', 'none' );
+define( '_AM_TINYCFG_RESIZE', 'Resize' );
+define( '_AM_TINYCFG_TRUE', 'true' );
+define( '_AM_TINYCFG_FALSE', 'false' );
+
+
+define( '_AM_TINYCFG_CONFIG', 'Configure' );
+define( '_AM_TINYCFG_CONFIGSAVE', 'Configuration for TinyMCE editor has been saved.' );
+
+define( '_AM_TINYCFG_VERIFYHTML', 'Verify HTML' );
+define( '_AM_TINYCFG_VERIFYHTMLDSC', 'This option enables or disables the element cleanup functionality. If you set this option to false, all element cleanup will be skipped but other cleanup functionality such as URL conversion will still be executed.<br />Default: <i>false</i>' );
+
+define( '_AM_TINYCFG_CONVERTURLS', 'Convert URLs' );
+define( '_AM_TINYCFG_CONVERTURLSDSC', 'This option enables you to control whether TinyMCE is to be clever and restore URLs to their original values. URLs are automatically converted (messed up) by default because the built-in browser logic works this way. There is no way to get the real URL unless you store it away. If you set this option to false it will try to keep these URLs intact.<br />Default: <i>false</i>' );
+
+define( '_AM_TINYCFG_RELATIVEURLS', 'Relative URLs' );
+define( '_AM_TINYCFG_RELATIVEURLSDSC', 'If this option is set to true, all URLs returned from the MCFileManager will be relative from the specified document_base_url. If it\'s set to false all URLs will be converted to absolute URLs. This option is set to true by default.' );
+
+define( '_AM_TINYCFG_FORCEBR', 'Force br elements' );
+define( '_AM_TINYCFG_FORCEBRDSC', 'If you set this option to true, TinyMCE will force BR elements on newlines instead of inserting paragraphs.<br />Default: <i>false</i>' );
+
+define( '_AM_TINYCFG_FORCEP', 'Force p elements' );
+define( '_AM_TINYCFG_FORCEPDSC', 'This option enables you to disable/enable the creation of paragraphs on return/enter in Mozilla/Firefox..<br />Default: <i>true</i>' );
+
+define( '_AM_TINYCFG_FORCEDRTBLCK', 'Forced root block' );
+define( '_AM_TINYCFG_FORCEDRTBLCKDSC', 'This option enables you to make sure that any non block elements or text nodes are wrapped in block elements.<br />Default: <i>p</i>' );
+
+define( '_AM_TINYCFG_SKIN', 'Select skin' );
+define( '_AM_TINYCFG_SKINDSC', '' );
+define( '_AM_TINYCFG_SKIN_DEFAULT', 'Default' );
+define( '_AM_TINYCFG_SKIN_CONTRAST', 'High Contrast' );
+define( '_AM_TINYCFG_SKIN_O2K7', 'O2K7' );
+
+define( '_AM_TINYCFG_SKINVAR', 'Select skin variant' );
+define( '_AM_TINYCFG_SKINVARDSC', '' );
+define( '_AM_TINYCFG_SKINVAR_BLACK', 'Black' );
+define( '_AM_TINYCFG_SKINVAR_SILVER', 'Silver' );
+
+?>
