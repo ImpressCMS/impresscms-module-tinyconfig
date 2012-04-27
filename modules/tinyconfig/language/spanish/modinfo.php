e5d0b299847dda3e7282e3238d3da0d2bf72222e
Juan Calzado <debianus@impresscms.org>
2012-04-27 18:51:13 +0000

    Spanish translation for tinyconfig
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/tinyconfig/trunk@24070 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ modules/tinyconfig/language/spanish/modinfo.php
@@ -0,0 +1,23 @@
+<?php
+/**
+* TinyConfig
+*
+* File: /language/spanish/modinfo.php
+*
+* @copyright	http://www.xoops.org/ The XOOPS Project
+* @copyright	XOOPS_copyrights.txt
+* @copyright	http://www.impresscms.org/ The ImpressCMS Project
+* @license		GNU General Public License (GPL)
+*				a copy of the GNU license is enclosed.
+* ----------------------------------------------------------------------------------------------------------
+* 				TinyConfig
+* @since		1.00
+* @author		McDonald
+* @version		$Id$
+*/
+
+define( '_MI_TINYCFG_DESCRIPTION', 'El módulo TinyConfig le permite configurar el editor WYSIWYG TinyMCE y asignar barras de botones para cada Grupo de usuarios.' );
+define( '_MI_TINYCFG_CONFIG', 'Configuración' );
+define( '_MI_TINYCFG_TOOLSETS', 'Toolsets' );
+define( '_MI_TINYCFG_SIMPLETOOLSETS', 'Toolsets simplificado' );
+?>
